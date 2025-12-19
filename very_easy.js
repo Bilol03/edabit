4491d19a7522d58e8fe2cd4c2f2fd9099671be4c
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-12-19 15:24:50 +0500

    very easydan 40ta masala bajarildi

---
+++ very_easy.js
@@ -0,0 +1,21 @@
+// function addition(a, b) {
+// 	return a + b
+// }
+
+// console.log(addition(2, 3), `5`)
+// console.log(addition(-3, -6), -9)
+// console.log(addition(7, 3), 10)
+// console.log(addition(88, 2), 90)
+
+
+
+/////////////
+
+// function convert(minutes) {
+// 	return minutes * 60
+// }
+
+// console.log(convert(6), 360)
+// console.log(convert(4), 240)
+// console.log(convert(8), 480)
+// console.log(convert(60), 3600)
