26d1ec896ea16d72499e1e3bd2179eda3512e348
Bilol <83568251+Bilol03@users.noreply.github.com>
2025-12-19 15:29:14 +0500

    changes

--- very_easy.js
+++ very_easy.js
@@ -1,6 +1,8 @@
-// function addition(a, b) {
-// 	return a + b
-// }
+
+// Return the Next Number from the Integer Passed
+function addition(a, b) {
+	return a + b
+}
 
 // console.log(addition(2, 3), `5`)
 // console.log(addition(-3, -6), -9)
@@ -9,11 +11,11 @@
 
 
 
-/////////////
+/// Conver minutes to seconds
 
-// function convert(minutes) {
-// 	return minutes * 60
-// }
+function convert(minutes) {
+	return minutes * 60
+}
 
 // console.log(convert(6), 360)
 // console.log(convert(4), 240)
@@ -21,13 +23,13 @@
 // console.log(convert(60), 3600)
 
 
-/////=========
+///// Find the next number of INT
 
 function addition(num) {
 	return ++num
 }
-console.log(addition(2), 3, "2 plus 1 equals 3.")
-console.log(addition(-9), -8, "-8 plus 1 equals -9.")
-console.log(addition(0), 1, "0 plus 1 equals 1.")
-console.log(addition(999), 1000, "999 plus 1 equals 1000.")
-console.log(addition(73), 74, "73 plus 1 equals 74.")
+// console.log(addition(2), 3, "2 plus 1 equals 3.")
+// console.log(addition(-9), -8, "-8 plus 1 equals -9.")
+// console.log(addition(0), 1, "0 plus 1 equals 1.")
+// console.log(addition(999), 1000, "999 plus 1 equals 1000.")
+// console.log(addition(73), 74, "73 plus 1 equals 74.")
